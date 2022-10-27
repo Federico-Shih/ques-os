9334ddf136c4861fecd791e0ae949f5af2c6b190
mrojaspelliccia <mrojaspelliccia@itba.edu.ar>
2022-10-27 10:22:19 -0300

    Small fix to buddy
    cheered him up a little

--- Kernel/memory_manager/buddy.c
+++ Kernel/memory_manager/buddy.c
@@ -13,16 +13,15 @@
 
 #define MAX_BLOCK_COUNT (MAX_ALLOC_LOG2 - MIN_ALLOC_LOG2)
 
-#define BLOCK_COUNT (MAX_ALLOC_LOG2 - MIN_ALLOC_LOG2 + 1)
 
-static list_t blocks[BLOCK_COUNT];
+static list_t orders[MAX_BLOCK_COUNT];
 static uint8_t currentBlocks;
 static size_t blockMaxSize;
 
 static list_t *base;
 
-static size_t getMinimumFittingBlock(size_t memorySize);
-static void addNodeToBlocks(list_t *blockList, list_t *node, uint8_t blockLevel);
+static size_t getMinimumFittingOrder(size_t memorySize);
+static void addNodeToOrder(list_t *blockList, list_t *node, uint8_t blockLevel);
 static int getFreeBlock(uint8_t minBucketRequired);
 static list_t *getBuddyBlock(list_t *node);
 static list_t *getBlockAddress(list_t *node);
@@ -32,44 +31,42 @@ static int log2(uint32_t number);
 
 void initializeMemoryManager (void *heap_base, unsigned int heap_size)
 {
-    if(heap_base == NULL)
-        return;
-    base = (list_t *) heap_base;
+    if(heap_base == NULL) return;
+    base = (list_t *)heap_base;
     blockMaxSize = heap_size;
 
-    currentBlocks = log2(heap_size) - MIN_ALLOC_LOG2 + 1;
+    currentBlocks = (int)log2(heap_size) - MIN_ALLOC_LOG2 + 1;
 
     if(currentBlocks > MAX_BLOCK_COUNT)
         currentBlocks = MAX_BLOCK_COUNT;
     
     for(int i = 0 ; i < currentBlocks; i++)
     {
-        initializeList(&blocks[i]);
-        blocks[i].free = 0;
-        blocks[i].order = i;
+        initializeList(&orders[i]);
+        orders[i].free = 0;
+        orders[i].order = i;
     }
-    addNodeToBlocks(&blocks[currentBlocks - 1], base, currentBlocks - 1);
+    addNodeToOrder(&orders[currentBlocks - 1], base, currentBlocks - 1);
 }
 
 void *malloc(uint64_t size)
 {
     size_t memNeeded = size + sizeof(list_t);
     
-    if(memNeeded == 0 || memNeeded > blockMaxSize + 1)
+    if(size == 0 || memNeeded > blockMaxSize + 1)
         return NULL;
 
-    uint8_t lowestFittingBlock = getMinimumFittingBlock(memNeeded);
-    int freeBlock = getFreeBlock(lowestFittingBlock);
+    uint8_t lowestFittingOrder = getMinimumFittingOrder(memNeeded);
+    int freeBlock = getFreeBlock(lowestFittingOrder);
 
-    if(freeBlock == -1)
-        return NULL;
+    if(freeBlock == -1) return NULL;
 
-    list_t *node = popFromList(&blocks[freeBlock]);
+    list_t *node = popFromList(&orders[freeBlock]);
 
-    for( ; lowestFittingBlock < freeBlock ; freeBlock--)
+    for( ; lowestFittingOrder < freeBlock ; freeBlock--)
     {
         node->order--;
-        addNodeToBlocks(&blocks[freeBlock - 1], getBuddyBlock(node), freeBlock - 1);
+        addNodeToOrder(&orders[freeBlock - 1], getBuddyBlock(node), freeBlock - 1);
     }
 
     node->free = 0;
@@ -79,10 +76,9 @@ void *malloc(uint64_t size)
 
 void free(void *block)
 {
-    if(block == NULL)
-        return;
+    if(block == NULL) return;
     
-    list_t *freeNode = (list_t *) block - 1;
+    list_t *freeNode = (list_t *)block - 1;
     freeNode->free = 1;
 
     list_t *freeBuddyBlock = getBuddyBlock(freeNode);
@@ -95,7 +91,7 @@ void free(void *block)
         freeBuddyBlock = getBuddyBlock(freeNode);
     }
 
-    pushToList(&blocks[freeNode->order], freeNode);
+    pushToList(&orders[freeNode->order], freeNode);
 }
 
 void memoryDump() {
@@ -107,10 +103,10 @@ void memoryDump() {
   printf("\nOrdenes con bloques libres:\n\n");
 
   for (int i = currentBlocks - 1; i >= 0; i--) {
-    list = &blocks[i];
+    list = &orders[i];
     if (!listIsEmpty(list)) {
       printf("    Orden %d\n", i + MIN_ALLOC_LOG2);
-      printf("    Bloques libres de tamaño 2^%d\n", i + MIN_ALLOC_LOG2);
+      printf("    Bloques libres de tamano 2^%d\n", i + MIN_ALLOC_LOG2);
       for (listAux = list->next, idx = 1; listAux != list;
            idx++, listAux = listAux->next) {
         if (listAux->free) {
@@ -124,54 +120,56 @@ void memoryDump() {
   printf("\nEspacio libre: %d\n\n", freeSpace);
 }
 
-static size_t getMinimumFittingBlock(size_t memorySize)
+static size_t getMinimumFittingOrder(size_t memorySize)
 {
-    size_t memoryBase2 = log2(memorySize);
+    size_t memoryBase2 = (size_t)log2(memorySize);
 
-    if(memoryBase2 < MIN_ALLOC_LOG2)
-        return 0;
+    if(memoryBase2 < MIN_ALLOC_LOG2) return 0;
 
     memoryBase2 -= MIN_ALLOC_LOG2;
 
-    if( memorySize && (memorySize & (memorySize -1)))
+    if( memorySize && !(memorySize & (memorySize - 1)))
         return memoryBase2;
     
     return memoryBase2 + 1;
 }
 
-void addNodeToBlocks(list_t *blockList, list_t *node, uint8_t blockOrder)
+void addNodeToOrder(list_t *blockList, list_t *node, uint8_t blockOrder)
 {
     node->order = blockOrder;
     node->free = 1;
     pushToList(blockList, node);
 }
+
 int getFreeBlock(uint8_t minBlockRequired)
 {
     uint8_t freeBlock;
 
-    for(freeBlock = minBlockRequired; freeBlock < currentBlocks && listIsEmpty(&blocks[freeBlock]); freeBlock++);
+    for(freeBlock = minBlockRequired; freeBlock < currentBlocks && listIsEmpty(&orders[freeBlock]); freeBlock++);
 
     if(freeBlock > currentBlocks)
         return -1;
     return freeBlock;
 }
+
 list_t *getBuddyBlock(list_t *node)
 {
-    uint8_t block = node->order;
-    uintptr_t currentOffset = (uintptr_t) node - (uintptr_t) base;
-    intptr_t newOffest = currentOffset ^ (1 << (MIN_ALLOC_LOG2 + block));
+    uint8_t order = node->order;
+    uintptr_t currentOffset = (uintptr_t)node - (uintptr_t)base;
+    intptr_t newOffest = currentOffset ^ (1 << (MIN_ALLOC_LOG2 + order));
 
-    return (list_t *)((uintptr_t) base + newOffest);
+    return (list_t *)((uintptr_t)base + newOffest);
 }
+
 list_t *getBlockAddress(list_t *node)
 {
-    uint8_t block = node->order;
-    uintptr_t mask = (1 << (MIN_ALLOC_LOG2 + block));
+    uint8_t order = node->order;
+    uintptr_t mask = (1 << (MIN_ALLOC_LOG2 + order));
     mask = ~mask;
-    uintptr_t currentOffset = (uintptr_t) node - (uintptr_t) base;
+    uintptr_t currentOffset = (uintptr_t)node - (uintptr_t)base;
     intptr_t newOffest = currentOffset & mask;
 
-    return (list_t *) (newOffest + (uintptr_t)base);
+    return (list_t *)(newOffest + (uintptr_t)base);
 }
 
 int log2(uint32_t number)
