f6f3fea122b3281616f1d99298bddc13fc2d2775
mrojaspelliccia <mrojaspelliccia@itba.edu.ar>
2022-10-27 16:35:04 -0300

    little comments

--- Kernel/memory_manager/buddy.c
+++ Kernel/memory_manager/buddy.c
@@ -6,14 +6,15 @@
 
 // min 16 bytes -> 2^4
 #define MIN_ALLOC_LOG2 4
-#define MIN_ALLOC ((size_t)1 << MIN_ALLOC_LOG2)
 
 // max 2GB -> 2^32
 #define MAX_ALLOC_LOG2 31
 
+// Usado para establecer la mayor cantidad de ordenes de bloques
 #define MAX_BLOCK_COUNT (MAX_ALLOC_LOG2 - MIN_ALLOC_LOG2)
 
-
+/* Array donde cada entrada representa un orden. Dentro, se guarda una lista para ese orden
+   La entrada 0 representa el orden maximo y la ultima el minimo */
 static list_t orders[MAX_BLOCK_COUNT];
 static uint8_t currentBlocks;
 static size_t blockMaxSize;
@@ -32,9 +33,12 @@ static int log2(uint32_t number);
 void initializeMemoryManager (void *heap_base, unsigned int heap_size)
 {
     if(heap_base == NULL) return;
+    //inicializo punteros generales
     base = (list_t *)heap_base;
-    blockMaxSize = heap_size;
 
+    //tamano maximo del orden mas grande
+    blockMaxSize = heap_size;
+    //cantidad de bloques disponibles (en total)
     currentBlocks = (int)log2(heap_size) - MIN_ALLOC_LOG2 + 1;
 
     if(currentBlocks > MAX_BLOCK_COUNT)
@@ -42,27 +46,35 @@ void initializeMemoryManager (void *heap_base, unsigned int heap_size)
     
     for(int i = 0 ; i < currentBlocks; i++)
     {
+        // inicializo cada posicion del array con una lista
+        // la cual esta libre y representa los bloques de orden i
         initializeList(&orders[i]);
         orders[i].free = 0;
         orders[i].order = i;
     }
+    // agrego el primer bloque
     addNodeToOrder(&orders[currentBlocks - 1], base, currentBlocks - 1);
 }
 
 void *malloc(uint64_t size)
 {
+    // necesito espacio necesario mas el tamano del nodo de la lista
     size_t memNeeded = size + sizeof(list_t);
     
     if(size == 0 || memNeeded > blockMaxSize + 1)
         return NULL;
 
+    //primero busco en que orden es mas correcto agregar ese bloque.
     uint8_t lowestFittingOrder = getMinimumFittingOrder(memNeeded);
+    //dentro de ese orden, busco un nodo en la lista
     int freeBlock = getFreeBlock(lowestFittingOrder);
 
     if(freeBlock == -1) return NULL;
 
+    
     list_t *node = popFromList(&orders[freeBlock]);
 
+    // divido el bloque libre(agrego nodos a la lista) en bloques mas pequeños hasta encontrar el bloque ideal
     for( ; lowestFittingOrder < freeBlock ; freeBlock--)
     {
         node->order--;
@@ -83,6 +95,7 @@ void free(void *block)
 
     list_t *freeBuddyBlock = getBuddyBlock(freeNode);
 
+    // si libero bloques, uno los que quedaron vacios
     while(freeNode->order != currentBlocks - 1 && freeBuddyBlock->order == freeNode->order && freeBuddyBlock->free)
     {
         removeFromList(freeBuddyBlock);
@@ -110,8 +123,8 @@ void memoryDump() {
       for (listAux = list->next, idx = 1; listAux != list;
            idx++, listAux = listAux->next) {
         if (listAux->free) {
-          printf("        Numero de bloque: %d\n", idx);
-          printf("        Estado: libre\n\n");
+          printf("    Numero de bloque: %d | ", idx);
+          printf("    Estado: libre\n");
           freeSpace += idx * (1 << (MIN_ALLOC_LOG2 + i));
         }
       }
