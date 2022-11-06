0f49bb3765fe4ec1afa91f8821e1d70b5fed6080
sh1ft <fedeshih@gmail.com>
2022-11-06 20:45:29 -0300

    new compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,28 @@
-#docker run -d -v "${PWD}:/root" --network="host" --security-opt seccomp:unconfined -ti --name $1  imnotgone/itba-so 
-docker start $1
-docker exec -it $1 make clean    -C /root/Toolchain
-docker exec -it $1 make all      -C /root/Toolchain
-docker exec -it $1 make clean    -C /root/
-docker exec -it $1 make all      -C /root/
-docker stop $1
-#docker rm $1
+#!/usr/bin/env bash
+
+if [ $1 == "docker"];
+  then
+    docker run -d -v "${PWD}:/root" --network="host" --security-opt seccomp:unconfined -ti --name $2 agodio/itba-so:1.0
+elif [ $1 == "buddy" ];
+  then
+    docker start $2
+    docker exec -it $2 make clean    -C /root/Toolchain
+    docker exec -it $2 make all      -C /root/Toolchain
+    docker exec -it $2 make clean    -C /root/
+    docker exec -it $2 make all MM=BUDDY       -C /root/
+    docker stop $2
+elif [ $1 == "standard" ];
+  then
+    docker start $2
+    docker exec -it $2 make clean    -C /root/Toolchain
+    docker exec -it $2 make all      -C /root/Toolchain
+    docker exec -it $2 make clean    -C /root/
+    docker exec -it $2 make all MM=BUDDY       -C /root/
+    docker stop $2
+elif [ $1 == "clean" ];
+  then
+    docker start $2
+    docker exec -it $2 make clean    -C /root/Toolchain
+    docker exec -it $2 make clean    -C /root/
+    docker stop $2
+fi
